aa125ae1656f8a5e3ad7423ada0f65795cde11d5
Olutimilehin Olushuyi <shuyi@shuyiolutimi.com>
2021-06-20 15:07:25 +0100

    Learnt events for forms such as keyup, keydown, focus, blur

--- exercises/29 - Events/forms.js
+++ exercises/29 - Events/forms.js
@@ -2,5 +2,32 @@ const signUpForm = document.querySelector(`[name='signup']`);
 
 signUpForm.addEventListener('submit', function (event) {
     event.preventDefault();
-    console.log(event.currentTarget);
+    console.log(event.currentTarget.name.value);
+    console.log(event.currentTarget.email.value);
+    console.log(event.currentTarget.agree.checked);
 });
+
+// Prevent a specific value from being submitted with a form
+
+signUpForm.addEventListener('submit', function (event) {
+    const name = event.currentTarget.name.value;
+    if (name.includes('Janet')) {
+        alert(`Sorry babe, no Janet's allowed here`);
+        event.preventDefault();
+    };
+})
+
+// 'keyup'
+// 'keydown'
+// 'focus'
+// 'blur'
+
+function logEvent(event) {
+    console.log(event.type);
+    console.log(event.currentTarget.value);
+};
+
+signUpForm.name.addEventListener('keyup', logEvent);
+signUpForm.name.addEventListener('keydown', logEvent);
+signUpForm.name.addEventListener('focus', logEvent);
+signUpForm.name.addEventListener('blur', logEvent);
